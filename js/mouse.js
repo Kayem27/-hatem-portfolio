c9cb043fddb9a4a7be9884ddae9a6ebecb9de7ab
Kayem <abdoulatuf.pro@gmail.com>
2023-04-08 18:11:30 +0200

    fix: bug d'affichage skills section

---
+++ js/mouse.js
@@ -0,0 +1,20 @@
+
+// const mouses = document.querySelectorAll(".mouse");
+
+// window.addEventListener("mousemove", (e) => {
+//   mouses.forEach((mouse) => {
+//     mouse.style.top = e.y + "px";
+//     mouse.style.left = e.x + "px";
+//   });
+// });
+
+// window.addEventListener("mousemove", (e) => {
+//   cursor.style.top = e.y + "px";
+//   cursor.style.left = e.x + "px";
+
+//   mouse1.style.top = e.y + "px";
+//   mouse1.style.left = e.x + "px";
+
+//   mouse2.style.top = e.y + "px";
+//   mouse2.style.left = e.x + "px";
+// })
